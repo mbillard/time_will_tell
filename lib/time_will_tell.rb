93e2a2e7fe1fbf904972486ba4f075fbc2ccea2b
Michel Billard <michel@mbillard.com>
2014-03-17 20:49:57 -0400

    Implemented TimeWillTell.date_range
    no railtie
    no activesupport (no I18n)

--- lib/time_will_tell.rb
+++ lib/time_will_tell.rb
@@ -9,6 +9,29 @@ class TimeWillTell
     { unit: 'minute', minutes_per_unit: 1 }
   ].freeze
 
+  SHORT_MONTH_NAMES = [nil, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'].freeze
+  LONG_MONTH_NAMES = [nil,
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ].freeze
+
+  DATE_RANGES_TEMPLATES = {
+    different_months_same_year: "%{from_month} %{from_day} %{sep} %{to_month} %{to_day}, %{year}",
+    different_years: "%{from_month} %{from_day}, %{from_year} %{sep} %{to_month} %{to_day}, %{to_year}",
+    same_month: "%{month} %{from_day} %{sep} %{to_day}, %{year}",
+    same_date: "%{month} %{from_day}, %{year}"
+  }.freeze
+
   def self.exact_distance_of_time_in_words(from_time, to_time, options = {})
     include_seconds         = options.fetch(:include_seconds, false)
 
@@ -45,5 +68,49 @@ class TimeWillTell
 
     distance_in_words.insert(-2, 'and') if distance_in_words.length > 2
     distance_in_words.join(' ')
+  end
+
+  def self.date_range(from_date, to_date, options = {})
+    format    = options.fetch(:format, :short)
+    separator = options.fetch(:separator, '—')
+
+    month_names = format.to_sym == :short ? SHORT_MONTH_NAMES : LONG_MONTH_NAMES
+
+    from_date, to_date = to_date, from_date if from_date > to_date
+    from_day   = from_date.day
+    from_month = month_names[from_date.month]
+    from_year  = from_date.year
+    to_day  = to_date.day
+
+    dates = { from_day: from_day, sep: separator }
+
+    if from_date == to_date
+      template = :same_date
+      dates.merge!(month: from_month, year: from_year)
+    elsif from_date.month == to_date.month && from_date.year == to_date.year
+      template = :same_month
+      dates.merge!(to_day: to_day, month: from_month, year: from_year)
+    else
+      to_month = month_names[to_date.month]
+
+      dates.merge!(from_month: from_month, to_month: to_month, to_day: to_day)
+
+      if from_date.year == to_date.year
+        template = :different_months_same_year
+        dates.merge!(year: from_year)
+      else
+        to_year = to_date.year
+
+        template = :different_years
+        dates.merge!(from_year: from_year, to_year: to_year)
+      end
+    end
+
+    date_range = DATE_RANGES_TEMPLATES[template].dup
+    dates.each do |key, value|
+      date_range.gsub!("%{#{key}}", value.to_s)
+    end
+
+    date_range
   end
 end
--- spec/time_will_tell_spec.rb
+++ spec/time_will_tell_spec.rb
@@ -41,4 +41,50 @@ describe TimeWillTell do
       ).to eq '4 days 15 minutes'
     end
   end
+
+  describe '.date_range' do
+    {
+      # short form
+      [Date.new(2012, 10, 3), Date.new(2012, 10, 8)] => 'Oct 3 — 8, 2012',
+      [Date.new(2013, 1, 30), Date.new(2013, 2, 5)] => 'Jan 30 — Feb 5, 2013',
+      [Date.new(2012, 12, 26), Date.new(2013, 1, 3)] => 'Dec 26, 2012 — Jan 3, 2013',
+      [Date.new(2013, 8, 24), Date.new(2013, 8, 24)] => 'Aug 24, 2013',
+      [Date.new(2014, 2, 10), Date.new(2015, 2, 10)] => 'Feb 10, 2014 — Feb 10, 2015',
+
+      # long form
+      [Date.new(2012, 10, 3), Date.new(2012, 10, 8), format: :long] => 'October 3 — 8, 2012',
+      [Date.new(2013, 1, 30), Date.new(2013, 2, 5), format: :long] => 'January 30 — February 5, 2013',
+      [Date.new(2012, 12, 26), Date.new(2013, 1, 3), format: :long] => 'December 26, 2012 — January 3, 2013',
+      [Date.new(2013, 8, 24), Date.new(2013, 8, 24), format: :long] => 'August 24, 2013',
+      [Date.new(2014, 2, 10), Date.new(2015, 2, 10), format: :long] => 'February 10, 2014 — February 10, 2015',
+    }.each do |args, expected|
+      it "outputs '#{expected}" do
+        from_date = args[0]
+        to_date   = args[1]
+        options   = args[2]
+
+        expect(
+          subject.date_range(*[from_date, to_date, options].compact)
+        ).to eq expected
+      end
+    end
+
+    it 'swaps from_date and to_date to get a positive range' do
+      from_date = Date.new(2014, 3, 17)
+      to_date   = Date.new(2000, 1, 1)
+
+      expect(
+        subject.date_range(from_date, to_date)
+      ).to eq 'Jan 1, 2000 — Mar 17, 2014'
+    end
+
+    it 'accepts a custom separator' do
+      from_date = Date.new(2013, 10, 31)
+      to_date   = Date.new(2013, 12, 25)
+
+      expect(
+        subject.date_range(from_date, to_date, separator: '~')
+      ).to eq 'Oct 31 ~ Dec 25, 2013'
+    end
+  end
 end
